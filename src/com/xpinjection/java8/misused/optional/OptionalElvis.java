dde5bd9986c45b8a79471fc4909e413fbd14847e
Sergii Zagryvyi <serzhcello@gmail.com>
2017-01-29 20:47:03 +0200

    start

--- src/com/xpinjection/java8/misused/optional/OptionalElvis.java
+++ src/com/xpinjection/java8/misused/optional/OptionalElvis.java
@@ -4,12 +4,35 @@ import com.xpinjection.java8.misused.Annotations.Good;
 import com.xpinjection.java8.misused.Annotations.Ugly;
 import com.xpinjection.java8.misused.User;
 
+import java.util.Optional;
+
 import static java.util.Optional.ofNullable;
 
 public class OptionalElvis {
 
     public static void main(String[] args) {
         User user = new User(1, "Vasia");
+        User userNull = null;
+
+        OptionalElvis elvis  = new OptionalElvis();
+        UsingOptionalIsPresent uop = elvis.new UsingOptionalIsPresent();
+        String name1 = uop.getUserName(user);
+        System.out.println(name1);
+
+        String name2 = uop.getUserName(userNull);
+        System.out.println(name2);
+
+        Optional<User> user21 = ofNullable(userNull);
+        System.out.println(user21);
+
+        UsingOrElse uoe = elvis.new UsingOrElse();
+        String userName = uoe.getUserName(user);
+        System.out.println(userName);
+
+        String userNameNull = uoe.getUserName(userNull);
+        System.out.println(userNameNull);
+
+        String name = ofNullable(userNull).map(User::getName).orElse("default Value");
     }
     @Ugly
     class BeforeJava8 {
